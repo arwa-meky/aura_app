0344596ce252145f9497f45776ae77fe01d112c7
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2026-01-21 02:26:02 +0200

    changes in the socket connection

--- lib/core/networking/socket_service.dart
+++ lib/core/networking/socket_service.dart
@@ -6,8 +6,15 @@ import 'package:socket_io_client/socket_io_client.dart' as IO;
 class SocketService {
   static late IO.Socket socket;
   static bool isConnected = false;
+  static bool _initialized = false;
 
   static void init(String token) {
+    if (_initialized) {
+      print('⚠️ Socket already initialized');
+      return;
+    }
+    _initialized = true;
+
     socket = IO.io(
       ApiConstants.baseUrl,
       IO.OptionBuilder()
@@ -32,11 +39,22 @@ class SocketService {
     });
 
     socket.onConnectError((data) => print("Socket Error: $data"));
+    // - This is the alert sent from the backend server which already comes from the
+    // - ds server
+    // - DS server => backend server => mobile
+    socket.on('alert response', (data) {
+      print(data);
+
+      // TODO:
+      // - Convert to AlertModel
+      // - Show notification
+    });
+
   }
 
   static void sendHealthData(Map<String, dynamic> data) {
     if (isConnected) {
-      // (اتأكد من الاسم مع الباك إند)
+      // - Emit the data read from the smartwatch to the backend server
       socket.emit('chat message', data);
       print("📡 Data emitted via Socket");
     } else {
